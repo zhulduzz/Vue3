dfea0a87ead96703f9adbd0abd78c6ba7a7bd731
zhulduzz <123058905+zhulduzz@users.noreply.github.com>
2025-10-18 13:25:56 +0500

    Add files via upload

---
+++ youtube/src/file/components/VideoCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+    title: string;
+    channel: string;
+    views: string;
+    thumbnail: string;
+}>();
+
+const emit = defineEmits(['liked'])
+
+function like() {
+  emit('liked')  // отправляем событие родителю
+}
+</script>
+
+<template>
+    <div class="video_card">
+        <p> title: {{ title }}</p>
+        <p> channel: {{ channel }}</p>
+        <p> views: {{  views }}</p>
+        <p> thumbnail: {{ thumbnail }}</p>
+        <button @click="$emit('liked')"> ❤■ </button>
+    </div>
+</template>
+
